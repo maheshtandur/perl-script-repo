37b15ea03401e67cc8434e2f02c869d885bbe723
Mahesh.Tandur <mahesh.tandur@gmail.com>
2013-04-25 22:14:02 +0200

    modifed by testing branch

--- add.pl
+++ add.pl
@@ -6,3 +6,7 @@ $sum = $num1 + $num2;
 print "Adding num1 + num2 = $sum \n";
 
 print "Well done added two number corretly \n";
+
+$mult = $num1 * $num2;
+
+print "Mutltiplying $num1 * $nume = $mult \n";
